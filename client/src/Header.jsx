2fa75b850b0bc8f2783fdc7716d45175e88ee8b0
pattma <pattarin.sine.m@gmail.com>
2023-10-14 17:55:37 +0700

    update header

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+              d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
             />
           </svg>
           <span className="font-bold text-xl">UniStay</span>
